f69e454ea5f54a4f90457a47383daca06546c675
Adrian <160585635+adrianm2801@users.noreply.github.com>
2024-05-21 16:25:31 +0800

    Add files via upload

---
+++ SIMPLE LANG/login.js
@@ -0,0 +1,6 @@
+document.querySelector("#svg").addEventListener("click",function(){
+    document.querySelector(".pop-up").classList.add("active");
+});
+document.querySelector(".pop-up .close-btn").addEventListener("click",function(){
+    document.querySelector(".pop-up").classList.remove("active");
+});
